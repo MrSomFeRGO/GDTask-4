aa6c73bff8ed37c1d7145a4bf0c5118a5e80f328
MrSomFeRGO <mr.somfergo@gmail.com>
2023-04-18 10:26:00 +0300

    Initial commit

---
+++ GDTask-4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine(shifr("test", 1700));
+
+static string shifr(string toShifr, int i)
+{
+    string result = "";
+    foreach (var element in toShifr)
+    {
+        int el_char = (int)element + i;
+        el_char = recursiya22(el_char);
+
+        result += (char)el_char;
+    }
+
+    return result;
+}
+
+static int recursiya22(int i)
+{
+    if (i > 255)
+    {
+        i -= 255;
+    }
+
+    if (i > 255)
+    {
+        return recursiya22(i);
+    }
+
+    return i;
+}
